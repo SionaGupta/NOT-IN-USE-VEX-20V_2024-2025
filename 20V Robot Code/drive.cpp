0b09499c711d48a9df4a979b90f421be44a85023
SionaGupta <siona.gupta00@gmail.com>
2024-07-25 18:06:45 -0700

    Create main.cpp and drive.cpp
    Created files for main.cpp and drive.cpp
    
    Main.cpp:
    config settings for lemlib including drive motors, tracking wheel config, and chassis creation... includes input scaling
    
    drive.cpp:
    drivetrain settings for two-stick arcade

---
+++ 20V Robot Code/drive.cpp
@@ -0,0 +1,20 @@
+#include "lemlib/api.hpp" // IWYU pragma: keep
+# include "main.cpp"
+
+//arcade drive
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+void opcontrol() {
+    // loop forever
+    while (true) {
+        // get left y and right x positions
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+
+        // move the robot
+        chassis.arcade(leftY, rightX);
+
+        // delay to save resources
+        pros::delay(25);
+    }
+}
